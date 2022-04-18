e200a6d153c77a700c905669c5bf2f3af89a0c78
ploshch <ploshchanski.a@gmail.com>
2022-04-18 23:15:34 +0300

    6

--- n30/code.js
+++ n30/code.js
@@ -15,7 +15,6 @@ for (let i = 0; i < img.length; i++) {
     imgItem.style.left = `${x[i]}px`;
     imgItem.style.top = `${y[i]}px`;
     imgItem.addEventListener('mousedown', dragStart);
-
 }
 
     console.log("All resources finished loading!");
@@ -29,24 +28,20 @@ function dragStart(eo) {
     let shiftX = eo.clientX - self.getBoundingClientRect().left; //eo.clientX - х курс. img.getBoundingClientRect().left - позиция элем. shiftX - от курс. до края 
     let shiftY = eo.clientY - self.getBoundingClientRect().top;
     document.body.appendChild(self);
-
+    eo.preventDefault();
 
     function dragDrop(pageX, pageY) {
         self.style.left = `${pageX - shiftX}px`;
         self.style.top = `${pageY - shiftY}px`;
-
     }
     function dragMove(eo) {
         dragDrop(eo.pageX, eo.pageY);
     }
     document.addEventListener('mousemove', dragMove);
     self.addEventListener('mouseup', dragEnd);
+    
     function dragEnd() {
         document.removeEventListener('mousemove', dragMove);
     };
-    self.ondragstart = function () {
-        return false;
-    };
-
 }
 
